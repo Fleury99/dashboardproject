dc0496ff17e240f1954e82b5003463178560d976
FleuryTheGodDev <kpemissiefleury@gmail.com>
2025-04-27 06:33:26 +0000

    Dashboard redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/',// Page racine
+        destination: '/dashboard', //Page cible
+        permanent: false, // Redirection temporaire
+      },
+    ];
+  },
 };
 
 export default nextConfig;
